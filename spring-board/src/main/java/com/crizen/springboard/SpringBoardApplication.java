555b5362b937f0078e45d3fc14eb78a983c43e0a
유호영 <hyyu0224@crizen.com>
2025-02-27 18:30:20 +0900

    feat: Init Spring boot, mariaDB-dev

---
+++ spring-board/src/main/java/com/crizen/springboard/SpringBoardApplication.java
@@ -0,0 +1,14 @@
+package com.crizen.springboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+public class SpringBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoardApplication.class, args);
+    }
+
+}
---
+++ spring-board/src/main/java/com/crizen/springboard/security/SecurityConfig.java
@@ -0,0 +1,6 @@
+package com.crizen.springboard.security;
+
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+
+public class SecurityConfig extends WebSecurityConfiguration {
+}
